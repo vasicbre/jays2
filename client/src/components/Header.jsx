5eb0583f8e712aaaf4d87e424f42e1ad82906f53
vasicnem <vasicnem@gmail.com>
2020-08-22 19:10:14 +0200

    Fix navbar padding

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top mb-2">
                 <a className="navbar-brand" href="">ETF</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -8,7 +8,7 @@ const ItemCard = ({item, history}) => {
     }
 
     return <div className="col-md-4">
-        <div className="card mt-1" onClick={ ()=> getItemDetails(item.id) }>
+        <div className="card mt-1 mb-1" onClick={ ()=> getItemDetails(item.id) }>
             <img src={item.image} alt="" className="card-img-top" />
             <div className="card-body">
                 <h5 className="card-title">{item.title}</h5>
