72f2087eddc6d9e671ed4b03d2cb87b44f3d22c9
vasicnem <vasicnem@gmail.com>
2020-08-30 13:11:25 +0200

    fix remove button

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -4,6 +4,13 @@ import { WithContext as ReactTags } from 'react-tag-input';
 
 class ItemCard extends Component {
 
+    constructor(props) {
+        super(props)
+    }
+    
+    componentDidMount() {
+    }
+
     getItemDetails = (id) => {
         this.props.history.push('/details?id='+id);
     }
@@ -15,14 +22,20 @@ class ItemCard extends Component {
         }
     });
 
+    removeButton() {
+        if (!this.props.mine) {
+            return <div></div>
+        }
+        return  <button type="button" className="close float-right" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+        </button>
+    }
     render() {
         return <div className="col-md-4">
             <div className="card mt-1 mb-1" onClick={ ()=> this.getItemDetails(this.props.item.id) }>
                 <img src={this.props.item.image} alt="" className="card-img-top" />
                 <div className="card-body">
-                    <button type="button" className="close float-right" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                    </button>
+                    {this.removeButton()}
                     <h5 className="card-title">{this.props.item.title}</h5>
                     <p className="card-text">{this.props.item.description}</p>
                     <div>
